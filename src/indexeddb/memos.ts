0cafb67cfc98366e13acce87d1bbf3f394566172
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-05-16 15:18:48 +0700

    add index page

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -1,16 +1,20 @@
-import Dexie from 'dexie'
+import Dexie from "dexie";
 
 export interface MemoRecord {
-    datetime: string
-    title: string
-    text: string
+    datetime: string;
+    title: string;
+    text: string;
 }
 
-const database = new Dexie('markdown-editor')
-database.version(1).stores({ memos: '&datetime' })
-const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
+const database = new Dexie("markdown-editor");
+database.version(1).stores({ memos: "&datetime" });
+const memos: Dexie.Table<MemoRecord, string> = database.table("memos");
 
 export const putMemo = async (title: string, text: string): Promise<void> => {
-    const datetime = new Date().toISOString()
-    await memos.put({ datetime, title, text })
-}
+    const datetime = new Date().toISOString();
+    await memos.put({ datetime, title, text });
+};
+
+export const getMemos = (): Promise<MemoRecord[]> => {
+    return memos.orderBy("datetime").reverse().toArray();
+};
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Header } from "../components/header";
 
+import { getMemos, MemoRecord } from "../indexeddb/memos";
+
+const { useState, useEffect } = React;
+
 const HeaderArea = styled.div`
     position: fixed;
     right: 0;
@@ -19,7 +23,35 @@ const Wrapper = styled.div`
     padding: 0 1rem;
 `;
 
+const Memo = styled.button`
+    display: block;
+    background-color: white;
+    border: 1px solid gray;
+    width: 100%;
+    padding: 1rem;
+    margin: 1rem 0;
+    text-align: left;
+`;
+
+const MemoTitle = styled.div`
+    font-size: 1rem;
+    margin-bottom: 0.5rem;
+`;
+
+const MemoText = styled.div`
+    font-size: 0.85rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`;
+
 export const History: React.FC = () => {
+    const [memos, setMemos] = useState<MemoRecord[]>([]);
+
+    useEffect(() => {
+        getMemos().then(setMemos);
+    }, []);
+
     return (
         <>
             <HeaderArea>
@@ -27,7 +59,14 @@ export const History: React.FC = () => {
                     <Link to="/editor">エディタに戻る</Link>
                 </Header>
             </HeaderArea>
-            <Wrapper>TODO: 履歴表示</Wrapper>
+            <Wrapper>
+                {memos.map((memo) => (
+                    <Memo key={memo.datetime}>
+                        <MemoTitle>{memo.title}</MemoTitle>
+                        <MemoText>{memo.text}</MemoText>
+                    </Memo>
+                ))}
+            </Wrapper>
         </>
     );
 };
